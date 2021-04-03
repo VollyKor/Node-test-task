a09d63d757859b4c6d74538e86e33b559e40f782
Warness <65484013+Warness@users.noreply.github.com>
2021-04-03 15:55:06 +0300

    add sequelize and other dependencies

---
+++ migrations/20210403092502-create-cashier.ts
@@ -0,0 +1,41 @@
+export default {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Cashiers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      age: {
+        type: Sequelize.INTEGER
+      },
+      yearsOfExpiriense: {
+        type: Sequelize.INTEGER
+      },
+      sex: {
+        type: Sequelize.INTEGER
+      },
+      cityWork: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Cashiers');
+  }
+};
---
+++ models/cashier.ts
@@ -0,0 +1,30 @@
+import { Model, DataType } from 'sequelize-typescript'
+
+export default (sequelize, DataTypes: typeof DataType) => {
+  class Cashier extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+
+  Cashier.init(
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      age: DataTypes.INTEGER,
+      yearsOfExpiriense: DataTypes.INTEGER,
+      sex: DataTypes.INTEGER,
+      cityWork: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Cashier',
+    },
+  );
+  return Cashier;
+};
